df9576d52ad8be055ad6a4f6327d422ff27316c6
shforeal <rivena14798@gmail.com>
2022-06-18 14:30:06 +0300

    feat: add marker on click

--- src/cmps/Map.jsx
+++ src/cmps/Map.jsx
@@ -5,15 +5,23 @@ import {Modal} from '../cmps/Modal'
 export const Map = () => {
   const center = useMemo(() => ({lat: 34, lng: -80}), [])
   const [pos, setPos] = useState(null)
+  const [markers, setMarkers] = useState([])
 
   const handleClick = ({latLng}) => {
     const pos = {lat: latLng.lat(), lng: latLng.lng()}
     setPos(pos)
+    setMarkers((prevMarkers) => [
+      ...prevMarkers,
+      {
+        title: 'The marker`s title will appear as a tooltip.',
+        name: 'test',
+        position: {lat: pos.lat, lng: pos.lng},
+      },
+    ])
   }
   //TODO: user click a loc open model to enter a name
   //Add it to the saved locs , put mark on it
   return (
-    // <section className="">
     <GoogleMap
       onClick={handleClick}
       zoom={10}
@@ -21,6 +29,14 @@ export const Map = () => {
       mapContainerClassName="map-container"
     >
       <Marker position={center} />
+      {markers.map((marker, idx) => (
+        <Marker
+          key={idx}
+          title={marker.title}
+          name={marker.name}
+          position={marker.position}
+        />
+      ))}
       {pos && <Modal pos={pos} />}
     </GoogleMap>
   )
--- src/cmps/Modal.jsx
+++ src/cmps/Modal.jsx
@@ -1,31 +1,37 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useForm} from '../hooks/useForm'
 import {locService} from '../services/loc.service'
 
 export const Modal = ({pos}) => {
   const [loc, handleChange, setLoc] = useForm(null)
+  const [isShow, setShow] = useState(false)
   console.log('got pos prop', pos)
 
   useEffect(() => {
     loadLoc()
-  }, [])
+  }, [pos])
 
   const loadLoc = () => {
     const loc = !pos ? locService.getEmptyLoc() : locService.createLoc(pos)
     console.log('loc', loc)
     setLoc(loc)
+    console.log('isShow', isShow)
+    setShow(true)
   }
 
   const onSaveLoc = async (ev) => {
     ev.preventDefault()
     const savedLoc = await locService.saveLoc(loc)
     console.log('savedLoc', savedLoc)
+    setShow(false)
   }
 
   if (!loc) return <div>Loading...</div>
 
   return (
-    <div className="modal pos-center pos-relative">
+    <div
+      className={'modal pos-center pos-relative' + (isShow ? ' show' : ' hide')}
+    >
       <h2 className="text-center u">Modal</h2>
       <form onSubmit={onSaveLoc} className="">
         <input
