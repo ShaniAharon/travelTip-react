6b0ecca18879f5213a75e39e29caed006475dd76
shforeal <rivena14798@gmail.com>
2022-06-18 10:29:24 +0300

    feat: start work on modal

---
+++ src/cmps/Modal.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+
+export const Modal = () => {
+  return (
+    //TODO: create a modal with form and input , when submit create a new loc
+    <div className="loc-list pos-relative">
+      <h2 className="text-center u">Your locs</h2>
+      <ul className="clean-list pos-center"></ul>
+    </div>
+  )
+}
