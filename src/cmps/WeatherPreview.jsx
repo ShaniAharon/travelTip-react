2112b7516426c23825a93914fa63707a138692fd
shforeal <rivena14798@gmail.com>
2022-06-19 16:48:03 +0300

    feat: extract the weather data , and display it

--- src/cmps/WeatherPreview.jsx
+++ src/cmps/WeatherPreview.jsx
@@ -5,19 +5,42 @@ import {locService} from '../services/loc.service'
 import {weatherService} from '../services/weather.service'
 
 export const WeatherPreview = ({pos}) => {
-  // const [isShow, setShow] = useState(false)
+  const [weatherData, setWeatherData] = useState(null)
 
   //get the center as a prop from the map cmp
   useEffect(() => {
     const centerLoc = locService.getCenterLoc()
-    weatherService.getWeather(centerLoc).then((res) => console.log(res))
+    weatherService.getWeather(centerLoc).then((res) => {
+      console.log(res)
+      setWeatherData(res)
+    })
   }, [pos])
 
-  // if (!pos) return <div>Loading...</div>
+  const prepareData = () => {
+    const {weather, main} = weatherData
+    const {temp, humidity} = main
+    const {description} = weather[0]
+    // const mains = Object.entries(main).map((item) => (
+    //   <li key={Math.random()}>{item}</li>
+    // ))
+    console.log('[temp, humidity, description]', [temp, humidity, description])
+    return [
+      `Temp: ${temp} `,
+      `Humidity: ${humidity}`,
+      `Description: ${description}`,
+    ]
+  }
 
+  if (!weatherData) return <div>Loading...</div>
   return (
     <section className="weather-preview">
       <h1>Weather</h1>
+      {/* <pre>{JSON.stringify([weather, main], 0, 4)}</pre> */}
+      <ul className="clean-list">
+        {prepareData().map((item) => (
+          <li key={Math.random()}>{item}</li>
+        ))}
+      </ul>
     </section>
   )
 }
